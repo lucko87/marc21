80b8c2df88b00f4a91ab4f3cfb2697a2a064884e
lucko87 <lucko87@gmail.com>
2017-03-21 00:24:02 +0100

    #2

--- src/hu/company/Main.java
+++ src/hu/company/Main.java
@@ -15,5 +15,15 @@ public class Main {
         System.out.println("A téglalap szépessége: " + width + "cm. A magassága: " + height + " cm.");
         System.out.println("A téglalap területe: " + (width * height) + " cm^2");
         System.out.println("A téglalap kerülete: " + (width * 2 + height * 2)+ " cm");
+
+        /*2. feladat
+        Létrehoztam a deciliter változót, és a kiíratáskor képlettel bemutattam, hogy milyen értékeket
+        kapunk, amikor néhány további térfogat mértékegységre váltjuk át.*/
+        float deciliter = 20;
+
+        System.out.println(deciliter + " dl = " + (deciliter * 100) + " ml");
+        System.out.println(deciliter + " dl = " + (deciliter * 10) + " cl");
+        System.out.println(deciliter + " dl = " + (deciliter / 10) + " l");
+        System.out.println(deciliter + " dl = " + (deciliter / 1000) + " hl");
     }
 }
